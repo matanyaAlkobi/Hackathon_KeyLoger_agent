b2b07eaf53c55b7db1d6eeb546256864b542626c
d.c! <daniel.c99999@gmail.com>
2025-02-18 16:58:18 +0200

    עידכון 18-02

--- new_projecct/Daniel Cohen/dc_Encryptor.py
+++ new_projecct/Daniel Cohen/dc_Encryptor.py
@@ -1,26 +1,44 @@
 import json
-class Encryptor:
+from abc import  ABC
+
+class Encryption:
 
     @staticmethod
-    def xor_encryption(text):
-        key = "a5555555ghfkglkdm"
-        encrypted_text = ""
+    def xor_encryption(text ,key: str):
+        text = str(text)
+        encrypted_text = []
+
         for i in range(len(text)):
-            encrypted_text += chr(ord(text[i]) ^ ord(key[i % len(key)]))
+            encrypted_text.append(ord(text[i]) ^ ord(key[i % len(key)]))
 
         return encrypted_text
 
+    @staticmethod
+    def xor_decryption(encrypted_text,key):
+        decrypted_text = ""
 
-a =Encryptor.xor_encryption("{aaaaaabc: 5}")
-print(a)
-# data = json.loads(a)
-# b = Encryptor.xor_encryption(data)
-print(Encryptor.xor_encryption(a))
+        for i in range(len(encrypted_text)):
+            decrypted_text += chr(encrypted_text[i] ^ ord(key[i % len(key)]))
+
+        return json.loads(decrypted_text)
+
+s = {"trsd}tgvsr":"ttttt"}
+
+data = json.dumps(s)
+print(type(data))
+
+b = Encryption()
+# enc =b.xor_decryption(data,"a")
+# print(type(enc))
+enc = Encryption.xor_encryption(data,"a")
+print(enc)
+dec = Encryption.xor_decryption(enc,"a")
+print(dec)
+# new = json.loads(dec)
+print(type(dec))
+# print(new['trsdtgvsr'])
 
 
 
-# print(data)
-# print(type(data))
-#
-#
-# {"machine": "data"}
+
+
